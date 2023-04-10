80938119f6883e72ac10bc89ac6841bc13a575ac
Anant <anant13u@gmail.com>
2023-04-10 16:34:50 +0530

    Created functions for the buttons to open their respective pages when clicked.

---
+++ Search Page/script.js
@@ -0,0 +1,12 @@
+
+const googleButton = document.getElementById('google-button')
+
+// googleButton.addEventListener('click',openGoogle())
+
+function openGoogle() {
+    window.open('https://google.com')
+}
+
+function openYoutube() {
+    window.open('https://youtube.com')
+}
