f3d64624c8bc63a9f314ffd1b0f4d9af600ed7c6
Anant <anant13u@gmail.com>
2023-04-11 16:34:04 +0530

    Added a condition to display an alert if the search text is blank or is the default text.

--- Search Page/script.js
+++ Search Page/script.js
@@ -11,22 +11,42 @@ twitterButton.addEventListener('click',openTwitter)
 
 function openGoogle() {
     const searchQuery = document.getElementById('search-box').innerHTML
+    if (searchQuery=='Enter the search keyword here'||searchQuery=='') {
+        alert('Please enter a search keyword')
+    }
+    else {
     window.open(`https://www.google.com/search?q=${searchQuery}`)
+    }
 }
 
 function openYoutube() {
     const searchQuery = document.getElementById('search-box').innerHTML
+    if (searchQuery=='Enter the search keyword here'||searchQuery=='') {
+        alert('Please enter a search keyword')
+    }
+    else {
     window.open(`https://www.youtube.com/results?search_query=${searchQuery}`)
+    }
 }
 
 function openReddit() {
     const searchQuery = document.getElementById('search-box').innerHTML
+    if (searchQuery=='Enter the search keyword here'||searchQuery=='') {
+        alert('Please enter a search keyword')
+    }
+    else {
     window.open(`https://www.reddit.com/search/?q=${searchQuery}`)
+    }
 }
 
 function openTwitter() {
     const searchQuery = document.getElementById('search-box').innerHTML
+    if (searchQuery=='Enter the search keyword here'||searchQuery=='') {
+        alert('Please enter a search keyword')
+    }
+    else {
     window.open(`https://twitter.com/search?q=${searchQuery}`)
+    }
 }
 
 
