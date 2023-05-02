fba65328c23395222911539dccc062645d52bbc9
Anant <anant13u@gmail.com>
2023-05-02 21:28:08 +0530

    Created a double-click event listener for each image in an array of images. This enables us to download an image on the page by double-clicking on it.

--- JS_Download_Image/script.js
+++ JS_Download_Image/script.js
@@ -1,27 +1,11 @@
-// const downloadLink = document.querySelector('.download-link');
-// const imgSrc = document.querySelector('.dwImage').src;
-
-// downloadLink.addEventListener('click', function() {
-//   download(imgSrc);
-// });
-
-// function download(url) {
-//   const link = document.createElement('a');
-//   link.href = url;
-//   link.download = 'Your Image';
-//   document.body.appendChild(link);
-//   link.click();
-//   document.body.removeChild(link);
-// }
-
 // select all image elements on the page
 const images = document.querySelectorAll('img');
+// const dwIcon = document.querySelector('.download-icon')
 
 // attach a click event listener to each image element
 images.forEach(image => {
-  image.addEventListener('click', () => {
-    // create a link element with the image source as the href
-    const link = document.createElement('a');
+  image.addEventListener("dblclick", () => {
+    const link = document.createElement('a'); // created a link element with the image source as the href
     link.href = image.src;
 
     // set the link attributes for downloading
@@ -29,18 +13,25 @@ images.forEach(image => {
     link.setAttribute('target', '_blank');
     link.setAttribute('rel', 'noopener noreferrer');
 
-    // create a download icon element and append it to the link element
-    const icon = document.createElement('i');
-    icon.classList.add('fa', 'fa-download', 'download-icon');
-    link.appendChild(icon);
-
-    // append the link element to the body and click it to start the download
-    document.body.appendChild(link);
     link.click();
-
-    // remove the link element from the body after the download is complete
-    link.addEventListener('load', () => {
-      document.body.removeChild(link);
-    });
   });
 });
+
+
+// This JavaScript code adds a double-click event listener to each image in an array 
+// of images (`images`). When an image is double-clicked, the code creates a new `<a>` 
+// element and sets its `href` attribute to the source (`src`) of the double-clicked 
+// image. This creates a hyperlink to the image.
+
+// The code then sets several attributes on the `<a>` element to enable downloading of 
+// the image. The `download` attribute is set to an empty string to indicate that the 
+// file should be downloaded instead of opened in the browser. The `target` attribute 
+// is set to `_blank` to open the download link in a new tab. The `rel` attribute is 
+// set to `noopener noreferrer` for security reasons.
+
+// Finally, the code programmatically clicks the `<a>` element using the `click()` 
+// method, which triggers the download of the image. This allows the user to download 
+// the image when they double-click on it.
+
+// Overall, this code adds a useful feature to a webpage that allows users to easily 
+// download images by double-clicking on them.
