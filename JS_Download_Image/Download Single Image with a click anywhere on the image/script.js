33f673a2d4000ab38d5e9644aa99dbc6dd7abfbb
Anant <anant13u@gmail.com>
2023-05-05 20:47:46 +0530

    Added dynamic image name to downloaded image in downloadButtons event listener
    In this commit, a dynamic name is added to the downloaded image in the downloadButtons event listener. The variable i is used to give a unique name to each image downloaded. This makes it easier for users to identify and organize downloaded images.

---
+++ JS_Download_Image/Download Single Image with a click anywhere on the image/script.js
@@ -0,0 +1,17 @@
+const downloadButtons = document.querySelectorAll('.image-container-child')
+// downloadButton.addEventListener('click', downloadImage)
+
+downloadButtons.forEach ((button, i) => {
+    button.addEventListener('click', () => {
+        const ourImage = button.querySelector('img')
+        const link = document.createElement('a')
+        link.href=ourImage.src;
+        link.download=''; // Added a name to the downloaded image using the variable i, so each image will have a unique name.
+        document.body.appendChild(link)
+        link.click()
+        document.body.removeChild(link) //Below line can also be used.
+        // link.remove()
+    })
+})
+
+// const zipDownloadButton = document.getElementById('zipdwButton')
