d817f1b036f5315ceea99e9cb443fcc8b4011208
Anant <anant13u@gmail.com>
2023-05-12 19:43:49 +0530

    This commit updates the code to allow for searching for movies based on user input change event and adds more movie details to be displayed. The previous implementation of the fetch functionality was also updated to reflect the changes. Additionally, the code now uses descriptive variables to represent each movie detail element, and comments were removed to improve readability.

--- Movie Search Page/script.js
+++ Movie Search Page/script.js
@@ -1,29 +1,43 @@
 const searchInput = document.getElementById('movie-search-input')
+const movieName = document.getElementById('movie-name')
 const movieYear = document.getElementById('movie-year')
 const movieGenre = document.getElementById('movie-genre')
-const fetchDetails = document.getElementById('fetch-button')
+const movieRating = document.getElementById('movie-rating')
+const movieReleased = document.getElementById('movie-released')
+const movieRuntime = document.getElementById('movie-runtime')
+const movieActors = document.getElementById('movie-actors')
+const moviePlot = document.getElementById('movie-plot')
+const moviePoster = document.getElementById('movie-poster')
+
+// const fetchDetails = document.getElementById('fetch-button')
+
 // const apiKey = '49c895a8'
 
 // searchInput.addEventListener('click', () => {
 //     searchInput.innerHTML=''
 // })
 
-fetchDetails.addEventListener('click', getMovieDetails)
-// searchInput.addEventListener('change', getMovieDetails)
+// fetchDetails.addEventListener('click', getMovieDetails)
+searchInput.addEventListener('input', getMovieDetails)
 
 function getMovieDetails() {
-    const movieName = searchInput.innerHTML
+    const searchName = searchInput.innerHTML
     console.log(movieName)
 
     // fetch(`https://www.omdbapi.com/?apikey=${apiKey}&t=${movieName}`)
-    fetch(`https://www.omdbapi.com/?apikey=49c895a8&t=${movieName}`)
+    fetch(`https://www.omdbapi.com/?apikey=49c895a8&t=${searchName}`)
         .then(Response => Response.json())
         .then(data => {
+            movieName.innerHTML=data.Title
+            // console.log(data.Title)
             movieYear.innerHTML=data.Year
-            console.log(data.Year)
             movieGenre.innerHTML=data.Genre
-            console.log(data.Genre)
-
+            movieRating.innerHTML=data.Rated
+            movieReleased.innerHTML=data.Released
+            movieRuntime.innerHTML=data.Runtime
+            movieActors.innerHTML=data.Actors
+            moviePlot.innerHTML=data.Plot
+            moviePoster.src = data.Poster
         })
 }
 
