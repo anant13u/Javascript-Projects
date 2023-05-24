39cf914553e6e6012d049ecce4fc13f1a7ddd2a2
Anant <anant13u@gmail.com>
2023-05-24 23:49:09 +0530

    Added code to fetch the city name when clicked on any area on the map.

--- Itinerary Generator/script.js
+++ Itinerary Generator/script.js
@@ -13,12 +13,22 @@ const map = new mapboxgl.Map({
 map.addControl(new mapboxgl.NavigationControl());
 
 // Event listener for city selection
-map.on('click', (e) => {
+map.on('click', async (e) => {
     const { lng, lat } = e.lngLat;
     console.log('Selected city coordinates:', lng, lat);
     // Perform actions with the selected city
     // For example: add a marker, store the coordinates, etc.
+    try {
+        const response = await fetch(`https://api.mapbox.com/geocoding/v5/mapbox.places/${lng},${lat}.json?types=place&access_token=${mapboxgl.accessToken}`);
+        const data = await response.json();
+        const cityName = data.features[0].text;
+        console.log('Selected city:', cityName);
+
+    } catch (error) {
+        console.log('Error: ',error)
+    }
   });
+
 
 
 // Default Public Token: pk.eyJ1IjoiYW5hbnQxM3UiLCJhIjoiY2xpMXJoZ2RvMDcyNjNkcGd5bnUwaG04aiJ9.l6C9K2TmrHuGYlHoE_-ZGA
