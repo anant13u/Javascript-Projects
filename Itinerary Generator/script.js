876b9986888a1369d0eaafbdd22095f9960c091f
Anant <anant13u@gmail.com>
2023-05-25 22:23:55 +0530

    The commit refactors the code handling the map click event and city selection.
    Previously, the code retrieved the geocoding data for the clicked location and created a new div element with the city name. The current code maintains this functionality but introduces clearer variable naming and comments.
    
    Additionally, the code for handling city selection and zooming has been improved. When a city is selected, the map is centered on the selected city and zoomed in to level 6. The 'selected' class is applied to the clicked city, and the 'selected' class is removed from other cities.
    
    The code for double-click zooming has been commented out for now.

--- Itinerary Generator/script.js
+++ Itinerary Generator/script.js
@@ -12,35 +12,72 @@ const map = new mapboxgl.Map({
 // Added navigation controls to the map
 map.addControl(new mapboxgl.NavigationControl());
 
-// Event listener for city selection
 map.on('click', async (e) => {
-    const { lng, lat } = e.lngLat;
+    // map.setZoom(5)
+    const { lng, lat } = e.lngLat; // Get the longitude and latitude of the clicked location
     console.log('Selected city coordinates:', lng, lat);
     // Perform actions with the selected city
     // For example: add a marker, store the coordinates, etc.
     try {
         const response = await fetch(`https://api.mapbox.com/geocoding/v5/mapbox.places/${lng},${lat}.json?types=place&access_token=${mapboxgl.accessToken}`);
-        const data = await response.json();
-        const place = data.features[0]
+        const data = await response.json(); // Retrieve the geocoding data for the selected location
+        const place = data.features[0]; // Get the first place result from the geocoding data
 
-        const cityName = document.createElement('div')
-        cityName.innerHTML = place.text;
+        const cityDiv = document.createElement('div'); // Create a new div element for the city
+        cityDiv.innerHTML = place.text; // Set the inner HTML of the div to the city name
+        cityDiv.classList.add('new-city'); // Add the 'new-city' class to the div
         console.log('Selected city:', place.text);
-        citiesContainer.appendChild(cityName)
-
-        // const address = document.createElement('div')
-        // address.innerHTML = place.properties.address;
-        // citiesContainer.appendChild(address)
+        citiesContainer.appendChild(cityDiv); // Append the city div to the cities container
 
     } catch (error) {
-        console.log('Error: ',error)
+        console.log('Error: ',error);
     }
-  });
+    const cities = document.querySelectorAll('.new-city');
+    cities.forEach(city => {
+        city.addEventListener('click', () => {
+            // Center the map on the selected city
+            const cityName = city.innerHTML; // Get the name of the selected city
+            fetch(`https://api.mapbox.com/geocoding/v5/mapbox.places/${cityName}.json?types=place&access_token=${mapboxgl.accessToken}`)
+                .then(response => response.json())
+                .then(data => {
+                    const lng = data.features[0].center[0]; // Get the longitude of the selected city
+                    console.log(lng);
+                    const lat = data.features[0].center[1]; // Get the latitude of the selected city
+                    console.log(lat);
+                    map.setCenter([lng,lat]); // Center the map on the selected city
+                    map.setZoom(6); // Zoom in to level 6
+
+                    cities.forEach(city => city.classList.remove('selected')); // Remove 'selected' class from all cities
+                    city.classList.add('selected'); // Add 'selected' class to the clicked city
+                });
+        });
+
+        // city.addEventListener('dblclick', () => {
+        //     map.setZoom(8)
+        // })
+    });
+});
+
+
+
+// cities.forEach(city => {
+//     city.addEventListener('click', () => {
+//         console.log(cities)
+//         console.log(cityName)
+
+//         fetch(`https://api.mapbox.com/geocoding/v5/mapbox.places/${cityName}.json?types=place&access_token=${mapboxgl.accessToken}`)
+
+
+//                 // Add a class to visually indicate the selected city
+
+//             })
+//             .catch(error => {
+//                 console.log('Error: ', error)
+//             })
+
+//     });
+// });
 
 
 
 // Default Public Token: pk.eyJ1IjoiYW5hbnQxM3UiLCJhIjoiY2xpMXJoZ2RvMDcyNjNkcGd5bnUwaG04aiJ9.l6C9K2TmrHuGYlHoE_-ZGA
-
-
-
-
