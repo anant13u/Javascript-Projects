580f650fdc1e749f10b4cb066dd425516d5cc2df
Anant <anant13u@gmail.com>
2023-05-26 18:34:09 +0530

    This commit adds a country filter to the geocoding request. This ensures that the results only include cities in India.

--- Itinerary Generator/script.js
+++ Itinerary Generator/script.js
@@ -13,13 +13,13 @@ const map = new mapboxgl.Map({
 map.addControl(new mapboxgl.NavigationControl());
 
 map.on('click', async (e) => {
-    // map.setZoom(5)
     const { lng, lat } = e.lngLat; // Get the longitude and latitude of the clicked location
     console.log('Selected city coordinates:', lng, lat);
     // Perform actions with the selected city
     // For example: add a marker, store the coordinates, etc.
     try {
-        const response = await fetch(`https://api.mapbox.com/geocoding/v5/mapbox.places/${lng},${lat}.json?types=place&access_token=${mapboxgl.accessToken}`);
+        // const response = await fetch(`https://api.mapbox.com/geocoding/v5/mapbox.places/${lng},${lat}.json?types=place&access_token=${mapboxgl.accessToken}`);
+        const response = await fetch(`https://api.mapbox.com/geocoding/v5/mapbox.places/${lng},${lat}.json?types=place&country=IN&access_token=${mapboxgl.accessToken}`);
         const data = await response.json(); // Retrieve the geocoding data for the selected location
         const place = data.features[0]; // Get the first place result from the geocoding data
 
@@ -37,47 +37,24 @@ map.on('click', async (e) => {
         city.addEventListener('click', () => {
             // Center the map on the selected city
             const cityName = city.innerHTML; // Get the name of the selected city
-            fetch(`https://api.mapbox.com/geocoding/v5/mapbox.places/${cityName}.json?types=place&access_token=${mapboxgl.accessToken}`)
+            fetch(`https://api.mapbox.com/geocoding/v5/mapbox.places/${cityName}.json?types=place&country=IN&access_token=${mapboxgl.accessToken}`)
                 .then(response => response.json())
                 .then(data => {
                     const lng = data.features[0].center[0]; // Get the longitude of the selected city
-                    console.log(lng);
+                    // console.log(lng);
                     const lat = data.features[0].center[1]; // Get the latitude of the selected city
-                    console.log(lat);
+                    // console.log(lat);
                     map.setCenter([lng,lat]); // Center the map on the selected city
-                    map.setZoom(6); // Zoom in to level 6
+                    map.setZoom(8); // Zoom in to level 6
 
                     cities.forEach(city => city.classList.remove('selected')); // Remove 'selected' class from all cities
                     city.classList.add('selected'); // Add 'selected' class to the clicked city
                 });
         });
-
         // city.addEventListener('dblclick', () => {
         //     map.setZoom(8)
         // })
     });
 });
-
-
-
-// cities.forEach(city => {
-//     city.addEventListener('click', () => {
-//         console.log(cities)
-//         console.log(cityName)
-
-//         fetch(`https://api.mapbox.com/geocoding/v5/mapbox.places/${cityName}.json?types=place&access_token=${mapboxgl.accessToken}`)
-
-
-//                 // Add a class to visually indicate the selected city
-
-//             })
-//             .catch(error => {
-//                 console.log('Error: ', error)
-//             })
-
-//     });
-// });
-
-
 
 // Default Public Token: pk.eyJ1IjoiYW5hbnQxM3UiLCJhIjoiY2xpMXJoZ2RvMDcyNjNkcGd5bnUwaG04aiJ9.l6C9K2TmrHuGYlHoE_-ZGA
