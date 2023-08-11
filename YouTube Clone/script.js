a2d949888e02ed2822f3a0c903abac1f718a0d0e
Anant <anant13u@gmail.com>
2023-08-11 21:36:08 +0530

    Introduced the constant resultsCount which represents the element in the search box taking input for the number of results user wishes to search for.

--- YouTube Clone/script.js
+++ YouTube Clone/script.js
@@ -1,19 +1,23 @@
-const searchBox = document.getElementById('search-box') 
+const searchBox = document.getElementById('search-box')
 const videosContainer = document.querySelector('.videos-container')
-const movieTrailer1 = document.getElementById('trailer1')
-const movieTrailer2 = document.getElementById('trailer2')
+const resultsCount = document.getElementById('results-count')
+// const movieTrailer1 = document.getElementById('trailer1')
+// const movieTrailer2 = document.getElementById('trailer2')
 
 const ytApiKey = 'AIzaSyCTUTvvpzzcodbELs4jfDdSNOybbmOglOU'
 
 const searchButton = document.querySelector('.search-button-container')
-searchButton.addEventListener('click',fetchTrailer)
+searchButton.addEventListener('click',fetchVideos)
 
-async function fetchTrailer() {
+async function fetchVideos() {
     try {
+        // Get the user-selected number of search results
+        const maxResults = resultsCount.value
+
         // Construct the URL for the API request
         const searchString = searchBox.innerHTML
         // console.log(`Trailer Search Input will be ${trailerName}`)
-        const apiURL = `https://www.googleapis.com/youtube/v3/search?key=${ytApiKey}&q=${searchString}&part=snippet&type=video&videoDefinition=high&maxResults=2`
+        const apiURL = `https://www.googleapis.com/youtube/v3/search?key=${ytApiKey}&q=${searchString}&part=snippet&type=video&videoDefinition=high&maxResults=${maxResults}`
         console.log(`searchString is ${searchString}`)
 
         // Send the API request and get the response
@@ -23,26 +27,16 @@ async function fetchTrailer() {
         // Get the video ID and URL for the video thumbnail
         const videoID1 = data.items[0].id.videoId;
         console.log(`Video ID 1 is ${videoID1}`)
-        const videoID2 = data.items[1].id.videoId;
-        console.log(`Video ID 2 is ${videoID2}`)
         const thumbnailUrl1 = data.items[0].snippet.thumbnails.high.url;
         console.log(`Thumbnail URL 1 is ${thumbnailUrl1}`);
-        const thumbnailUrl2 = data.items[1].snippet.thumbnails.high.url;
-        console.log(`Thumbnail URL 2 is ${thumbnailUrl2}`);
 
         // Construct the URL for the video player
         const embedUrl1 = `https://www.youtube.com/embed/${videoID1}`;
-        // console.log(`The Video URL is ${videoUrl2}`)
-        const embedUrl2 = `https://www.youtube.com/embed/${videoID2}`;
-        console.log(`Embed URL is ${thumbnailUrl2}`)
 
         const iframeHtml1 = `<iframe width="560" height="315" src="${embedUrl1}" frameborder="0" style='border-radius:15px' allowfullscreen></iframe>`;
         console.log(`iFrameHtml is ${iframeHtml1}`)
-        const iframeHtml2 = `<iframe width="560" height="315" src="${embedUrl2}" frameborder="0" style='border-radius:15px' allowfullscreen></iframe>`;
-        console.log(`iFrameHtml is ${iframeHtml2}`)
 
         movieTrailer1.innerHTML = iframeHtml1
-        movieTrailer2.innerHTML = iframeHtml2
 
         // return { videoUrl, thumbnailUrl };
     } catch (error) {
