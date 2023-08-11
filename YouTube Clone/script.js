b53cd69ce572ac3021f2a2f19b37de88b7eac0cb
Anant <anant13u@gmail.com>
2023-08-12 00:52:42 +0530

    Revised how the maxResults value is retrieved and used to improve clarity. Improved the handling of API responses by dynamically generating and displaying multiple video elements within the videosContainer. Removed unnecessary commented-out code and debug logs for cleaner code readability.

--- YouTube Clone/script.js
+++ YouTube Clone/script.js
@@ -1,8 +1,6 @@
 const searchBox = document.getElementById('search-box')
 const videosContainer = document.querySelector('.videos-container')
 const resultsCount = document.getElementById('results-count')
-// const movieTrailer1 = document.getElementById('trailer1')
-// const movieTrailer2 = document.getElementById('trailer2')
 
 const ytApiKey = 'AIzaSyCTUTvvpzzcodbELs4jfDdSNOybbmOglOU'
 
@@ -12,33 +10,41 @@ searchButton.addEventListener('click',fetchVideos)
 async function fetchVideos() {
     try {
         // Get the user-selected number of search results
-        const maxResults = resultsCount.value
+        const maxResults = resultsCount.innerHTML
+        // const maxResults = parseInt(resultsCount.innerHTML)
+        console.log(maxResults)
 
         // Construct the URL for the API request
         const searchString = searchBox.innerHTML
         // console.log(`Trailer Search Input will be ${trailerName}`)
         const apiURL = `https://www.googleapis.com/youtube/v3/search?key=${ytApiKey}&q=${searchString}&part=snippet&type=video&videoDefinition=high&maxResults=${maxResults}`
-        console.log(`searchString is ${searchString}`)
+        // console.log(`searchString is ${searchString}`)
 
         // Send the API request and get the response
         const response = await fetch(apiURL)
         const data = await response.json()
 
-        // Get the video ID and URL for the video thumbnail
-        const videoID1 = data.items[0].id.videoId;
-        console.log(`Video ID 1 is ${videoID1}`)
-        const thumbnailUrl1 = data.items[0].snippet.thumbnails.high.url;
-        console.log(`Thumbnail URL 1 is ${thumbnailUrl1}`);
+        videosContainer.innerHTML=''
 
-        // Construct the URL for the video player
-        const embedUrl1 = `https://www.youtube.com/embed/${videoID1}`;
-
-        const iframeHtml1 = `<iframe width="560" height="315" src="${embedUrl1}" frameborder="0" style='border-radius:15px' allowfullscreen></iframe>`;
-        console.log(`iFrameHtml is ${iframeHtml1}`)
-
-        movieTrailer1.innerHTML = iframeHtml1
-
-        // return { videoUrl, thumbnailUrl };
+        for (let i=0; i<data.items.length;i++) {
+            // Get the video ID and URL for the video thumbnail
+            const videoID = data.items[i].id.videoId;
+            // console.log(`Video ID 1 is ${videoID}`)
+    
+            // const thumbnailUrl = data.items[0].snippet.thumbnails.high.url;
+            // console.log(`Thumbnail URL 1 is ${thumbnailUrl}`);
+    
+            // Construct the URL for the video player
+            const embedUrl = `https://www.youtube.com/embed/${videoID}`;
+    
+            const iframeHtml = `<iframe width="560" height="315" src="${embedUrl}" frameborder="0" style='border-radius:15px' allowfullscreen></iframe>`;
+            // console.log(`iFrameHtml is ${iframeHtml1}`)
+    
+            const videoElement = document.createElement('div')
+            videoElement.innerHTML=iframeHtml
+    
+            videosContainer.appendChild(videoElement)
+        }
     } catch (error) {
         console.log(error);
   }
