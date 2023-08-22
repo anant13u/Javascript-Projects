0f3f30a52d6abdc851448e074665e8bb4a7cb4c7
Anant <anant13u@gmail.com>
2023-08-22 23:51:08 +0530

    Added code to perform an input validation check to ensure that maxResults is not greater than 20. If the validation fails, display an alert message instructing the user to enter a number less than or equal to 20.

--- YouTube Clone/script.js
+++ YouTube Clone/script.js
@@ -8,6 +8,7 @@ const searchButton = document.querySelector('.search-button-container')
 searchButton.addEventListener('click',fetchVideos)
 
 searchBox.addEventListener('click', clearSearchBox)
+searchBox.addEventListener('focus', clearSearchBox)
 
 function clearSearchBox() {
     if (searchBox.innerHTML=='Search') {
@@ -16,6 +17,7 @@ function clearSearchBox() {
 }
 
 resultsCount.addEventListener('click', clearResultsCounter)
+resultsCount.addEventListener('focus', clearResultsCounter)
 
 function clearResultsCounter() {
     if (resultsCount.innerHTML=='Results Count') {
@@ -27,39 +29,43 @@ async function fetchVideos() {
     try {
         // Get the user-selected number of search results
         const maxResults = resultsCount.innerHTML
-        // const maxResults = parseInt(resultsCount.innerHTML)
-        console.log(maxResults)
-
-        // Construct the URL for the API request
-        const searchString = searchBox.innerHTML
-        // console.log(`Trailer Search Input will be ${trailerName}`)
-        const apiURL = `https://www.googleapis.com/youtube/v3/search?key=${ytApiKey}&q=${searchString}&part=snippet&type=video&videoDefinition=high&maxResults=${maxResults}`
-        // console.log(`searchString is ${searchString}`)
-
-        // Send the API request and get the response
-        const response = await fetch(apiURL)
-        const data = await response.json()
-
-        videosContainer.innerHTML=''
-
-        for (let i=0; i<data.items.length;i++) {
-            // Get the video ID and URL for the video thumbnail
-            const videoID = data.items[i].id.videoId;
-            // console.log(`Video ID 1 is ${videoID}`)
+        if (maxResults>20) {
+            alert('Please enter a number less than or equal to 20.')
+        } else {
+            // const maxResults = parseInt(resultsCount.innerHTML)
+            console.log(maxResults)
     
-            // const thumbnailUrl = data.items[0].snippet.thumbnails.high.url;
-            // console.log(`Thumbnail URL 1 is ${thumbnailUrl}`);
+            // Construct the URL for the API request
+            const searchString = searchBox.innerHTML
+            // console.log(`Trailer Search Input will be ${trailerName}`)
+            const apiURL = `https://www.googleapis.com/youtube/v3/search?key=${ytApiKey}&q=${searchString}&part=snippet&type=video&videoDefinition=high&maxResults=${maxResults}`
+            // console.log(`searchString is ${searchString}`)
     
-            // Construct the URL for the video player
-            const embedUrl = `https://www.youtube.com/embed/${videoID}`;
+            // Send the API request and get the response
+            const response = await fetch(apiURL)
+            const data = await response.json()
     
-            const iframeHtml = `<iframe width="560" height="315" src="${embedUrl}" frameborder="0" style='border-radius:15px' allowfullscreen></iframe>`;
-            // console.log(`iFrameHtml is ${iframeHtml1}`)
+            videosContainer.innerHTML=''
     
-            const videoElement = document.createElement('div')
-            videoElement.innerHTML=iframeHtml
-    
-            videosContainer.appendChild(videoElement)
+            for (let i=0; i<data.items.length;i++) {
+                // Get the video ID and URL for the video thumbnail
+                const videoID = data.items[i].id.videoId;
+                // console.log(`Video ID 1 is ${videoID}`)
+        
+                // const thumbnailUrl = data.items[0].snippet.thumbnails.high.url;
+                // console.log(`Thumbnail URL 1 is ${thumbnailUrl}`);
+        
+                // Construct the URL for the video player
+                const embedUrl = `https://www.youtube.com/embed/${videoID}`;
+        
+                const iframeHtml = `<iframe width="560" height="315" src="${embedUrl}" frameborder="0" style='border-radius:15px' allowfullscreen></iframe>`;
+                // console.log(`iFrameHtml is ${iframeHtml1}`)
+        
+                const videoElement = document.createElement('div')
+                videoElement.innerHTML=iframeHtml
+        
+                videosContainer.appendChild(videoElement)
+            }
         }
     } catch (error) {
         console.log(error);
