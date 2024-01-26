7728a44ecff456dfc7fb3dd401f0a9307c5b672a
Anant Upadhyay <anant13u@gmail.com>
2024-01-26 14:54:29 +0530

    Removed variable i as it's not being used anywhere in the code for now. Also added useful comments for better understanding of the script.

--- JS_Download_Image/Download Single Image with a click anywhere on the image/script.js
+++ JS_Download_Image/Download Single Image with a click anywhere on the image/script.js
@@ -1,14 +1,23 @@
+// Select all elements with the class 'image-container-child' and store them in downloadButtons array
 const downloadButtons = document.querySelectorAll('.image-container-child')
-// downloadButton.addEventListener('click', downloadImage)
 
-downloadButtons.forEach ((button, i) => {
+// Iterate over each element in the downloadButtons array
+downloadButtons.forEach (button => {
+    // Add an event listener to each button in the downloadButtons array
     button.addEventListener('click', () => {
+        // Find the <img> element within the clicked button and store it in ourImage
         const ourImage = button.querySelector('img')
+        // Create an <a> element to act as a download link
         const link = document.createElement('a')
+        // Set the href attribute of the link to the source URL of the <img> element
         link.href=ourImage.src;
-        link.download=''; // Added a name to the downloaded image using the variable i, so each image will have a unique name.
-        document.body.appendChild(link)
-        link.click()
+
+        link.download=''; // Set the download attribute of the link to an empty string
+        document.body.appendChild(link) // Append the link to the document body
+
+        link.click() // Programmatically trigger a click event on the link to initiate the download
+        
+        // Remove the link element from the document body after the download completes
         document.body.removeChild(link) //Below line can also be used.
         // link.remove()
     })
