d2c0f8b2e1e3613c47ac07530ea3e365f629895d
Anant <anant13u@gmail.com>
2024-11-08 19:24:46 +0530

    Adding code to toggle hamburger menu's visibility.

---
+++ ACI/script.js
@@ -0,0 +1,7 @@
+const hamburgerIcon = document.getElementsByClassName('hamburger-icon')
+
+addEventListener
+function toggleMenu() {
+    const navbar = document.getElementById("navbar");
+    navbar.classList.toggle("show");
+}
